2cc9af34389728f528eb110e883acb6b4c8e8acc
zztkm <takumi.basket1682@gmail.com>
2022-05-25 14:10:30 +0900

    Add: cmd version flag

--- cmd/toolformation/main.go
+++ cmd/toolformation/main.go
@@ -1,13 +1,31 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 
 	"github.com/zztkm/toolformation"
 )
 
+const (
+	name    = "taskformation"
+	version = "0.1.0"
+)
+
+var (
+	versionFlag = flag.Bool("version", false, "print version")
+)
+
 // TODO: いずれ cobra かなんか使ったCLIにする1
 func main() {
+	flag.Parse()
+
+	if *versionFlag {
+		fmt.Printf("%s %s", name, version)
+		return
+	}
+
 	t, err := toolformation.New("ToolFormation.yml")
 	if err != nil {
 		log.Fatal(err)
