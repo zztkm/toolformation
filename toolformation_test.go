0c891ddfdf29790de4cf0675e7a456c6ee516a03
zztkm <takumi.basket1682@gmail.com>
2022-05-25 13:12:37 +0900

    ✅ Add: TestCheck

--- toolformation_test.go
+++ toolformation_test.go
@@ -1,1 +1,22 @@
 package toolformation
+
+import "testing"
+
+func TestCheck(t *testing.T) {
+	var tests = []struct {
+		name string
+		args string
+		want int
+	}{
+		{"normal", "which", 0},
+		{"abnormal", "dfafaff0921jfad", 1},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := check(tt.args); got != tt.want {
+				t.Errorf("check() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
